1916f5c9658fd3b0d98aef7948d74304af7ce582
Abdurashid001 <yetmishovabdurashid13@gmail.com>
2023-10-03 16:26:26 +0500

    "lhdc"

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+    }
+}
